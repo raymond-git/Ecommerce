4a085433bb3c07f74322ba5466f42bea2df41a95
Raymond Huang <raymondhuang615@yahoo.com>
2023-05-09 19:56:08 -0700

    firebase setup

---
+++ ecommerce/functions/server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+require('dotenv').config();
+
+app.use(express.static("public"));
+app.use(express.json());
+
+const port = process.env.PORT;
+const stripe = require('stripe')(process.env.SECRET_KEY);
+
+app.post('/create-checkout-session', async (req, res) => {
+  const items = req.body.items;
+  const line_items = items.map(item => {
+    return {
+      price_data: {
+        currency: 'usd',
+        product_data: {
+          name: item.itemProduct.title,
+          images: [item.itemProduct.image]
+        },
+        unit_amount: item.itemProduct.price * 100,
+      },
+      adjustable_quantity: {
+        enabled: true,
+        minimum: 1,
+        maximum: 100,
+      },
+      quantity: item.itemQuantity,
+    }
+  });
+  const session = await stripe.checkout.sessions.create({
+    line_items,
+    mode: 'payment',
+    invoice_creation: { enabled: true },
+    allow_promotion_codes: true,
+    payment_method_types: ["card"],
+    success_url: process.env.STRIPE_PAYMENT_CHECKOUT_SUCCESS,
+    cancel_url: process.env.BACK_TO_CART,
+  });
+  res.json({ url: session.url });
+});
+
+app.listen(port, () => console.log(`Node server listening on port ${process.env.PORT}!`));
