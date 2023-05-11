1e3ea715b1e3d3e8d162885f74de67bad9a0cb25
Raymond Huang <raymondhuang615@yahoo.com>
2023-05-10 23:25:40 -0700

    added cors

--- ecommerce/src/server/server.js
+++ ecommerce/src/server/server.js
@@ -5,6 +5,12 @@ require('dotenv').config();
 app.use(express.static("public"));
 app.use(express.json());
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 const port = process.env.PORT || 8000;
 const stripe = require('stripe')(process.env.SECRET_KEY);
 
