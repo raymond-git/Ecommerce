ecaa7cedcd8bf05e77b672d662edfb1ef538ad4e
Raymond Huang <raymondhuang615@yahoo.com>
2023-05-21 13:45:08 -0700

    Finalize Website

--- ecommerce/src/components/paymentUnsuccessful.js
+++ ecommerce/src/components/paymentUnsuccessful.js
@@ -33,15 +33,15 @@ const PaymentUnsuccessful = () => {
             <div className="mt-20">
                 <div className="flex flex-col items-center gap-20">
                     <img className="h-32 md:h-36 lg:h-40" src={dataUrl}></img>
-                    <h1 className="font-sans font-bold lg:font-bold text-xl md:text-2xl lg:text-4xl payment-unsuccessful-text-color">Payment was Unsuccessful</h1>
+                    <h1 className="robotoFont font-bold lg:font-bold text-2xl md:text-3xl lg:text-4xl payment-unsuccessful-text-color">Payment was Unsuccessful</h1>
                 </div>
                 <div className="mt-4">
                     <div className="flex flex-col items-center lg:gap-8">
-                        <p className="font-medium lg:font-medium text-lg md:text-xl lg:text-2xl">We're here to help.</p>
+                        <p className="robotoFont lg:font-medium text-lg md:text-xl lg:text-2xl">We're here to help.</p>
                         <p className="p-14 lg:p-0 text-center">We apologize for the inconvenience. Please check your payment details and contact us for assistance. Thank you for your patience and understanding. </p>
                         <div className=" lg:flex justify-between gap-8">
-                            <Link to={`/products`}><button className="btn btn-primary btn-lg btn-block font-sans h-10 flex items-center justify-center lg:h-12 lg:btn-lg lg:w-56 mt-4 rounded-3xl">Shopping Page</button></Link>
-                            <button onClick={handleCheckout} className="btn btn-primary btn-lg btn-block font-sans h-10 flex items-center justify-center lg:h-12 lg:btn-lg lg:w-56 mt-4 rounded-3xl">View Checkout</button>
+                            <Link to={`/products`}><button className="btn btn-primary btn-lg btn-block robotoFont h-12 flex items-center justify-center lg:h-12 lg:btn-lg lg:w-56 mt-4 rounded-3xl">Shopping Page</button></Link>
+                            <button onClick={handleCheckout} className="btn btn-primary btn-lg btn-block robotoFont h-12 flex items-center justify-center lg:h-12 lg:btn-lg lg:w-56 mt-4 rounded-3xl">View Checkout</button>
                         </div>
                     </div>
                 </div>
